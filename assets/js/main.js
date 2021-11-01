97589172567f7372ca9ae4fc4543f5f634fc3b41
Mokusei <mokuseidev@gmail.com>
2021-11-01 09:22:49 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,62 @@
-uuidv4 = function() {
-	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-		var r = Math.random() * 16 | 0,
-		v = c == 'x' ? r: (r & 0x3 | 0x8);
-		return v.toString(16);
-	});
-};
+const name = document.getElementById("name");
+const description = document.getElementById("description");
+const min_engine_version = document.getElementById("min_engine_version");
+const type = document.getElementById("type");
+const generate = document.getElementById("generate");
+const output = document.getElementById("output");
+const copyToClipboard = document.getElementById("copyToClipboard");
+const downloadFile = document.getElementById("downloadFile");
 
-code = function() {
-var code = `
-{
-	"format_version": 2,
-	"header": {
-		"name": "${document.getElementById("name").value}",
-		"description": "${document.getElementById("description").value}",
-		"uuid": "${uuidv4()}",
-		"version": [${document.getElementById("version").value}],
-		"min_engine_version": [1, 16, 0]
-	},
-	"modules": [
-		{
-			"type": "${(document.getElementById("type").value)}",
-			"uuid": "${uuidv4()}",
-			"version": [${document.getElementById("version").value}]
-		}
-	]
-}
-`;
-}
-
-document.getElementById("generate").addEventListener("click", () => {
-	document.getElementById("output").value = code();
+generate.addEventListener("click", () => {
+	output.value = generateJSON(generateUUID(), generateUUID(), generateUUID());
 });
 
-document.getElementById("copyToClipboard").addEventListener("click", () => {
-	document.getElementById("output").select();
-	document.getElementById("output").setSelectionRange(0, document.getElementById("output").value.length);
+copyToClipboard.addEventListener("click", () => {
+	output.select();
+	output.setSelectionRange(0, output.value.length);
 	document.execCommand("copy");
 });
 
-document.getElementById("downloadFile").addEventListener("click", () => {
-	var f = document.getElementById("output").value;
-	f = new Blob([f], {
-		type: "text/plain;charset=utf-8"
+downloadFile.addEventListener("click", () => {
+	const a = document.createElement('a');
+	const file = new Blob([output.value], {
+		type: "text/plain"
 	});
-	var c = document.createElement("a");
-	c.download = "manifest.json";
-	c.innerHTML = "Save";
-	null != window.webkitURL ? c.href = window.webkitURL.createObjectURL(f): (c.href = window.URL.createObjectURL(f), c.onclick = destroyClickedElement, c.style.display = "none", document.body.appendChild(c));
-	c.click();
+	a.href = URL.createObjectURL(file);
+	a.download = "manifest.json";
+	a.click();
+	URL.revokeObjectURL(a.href);
 });
+
+generateUUID = function() {
+	var d = new Date().getTime();
+	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = (d + Math.random() * 16) % 16 | 0;
+		d = Math.floor(d / 16);
+		return (c == 'x' ? r: (r & 0x3 | 0x8)).toString(16);
+	});
+
+	return uuid;
+};
+
+generateJSON = function(uuid1, uuid2, uuid3) {
+	let manifest = {};
+
+	manifest.format_version = 2;
+	manifest.header = {};
+	manifest.header.name = name.value;
+	manifest.header.description = description.value;
+	manifest.header.uuid = uuid1;
+	manifest.header.version = [1, 0, 0];
+	manifest.header.min_engine_version = JSON.parse(min_engine_version.value);
+	manifest.modules = [{}];
+	manifest.modules[0].type = type.value;
+	manifest.modules[0].uuid = uuid2;
+	manifest.modules[0].version = [1, 0, 0];
+
+	if (type.value === "data") manifest.dependencies = [{}];
+	if (type.value === "data") manifest.dependencies[0].uuid = uuid3;
+	if (type.value === "data") manifest.dependencies[0].version = [1, 0, 0];
+
+	return JSON.stringify(manifest, null, "\t");
+}
